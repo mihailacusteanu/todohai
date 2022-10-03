b71c0f786c3518ba732d7271aa629689d05b9a93
mihailacusteanu <mihailacusteanu@gmail.com>
2022-10-03 21:54:52 +0300

    chore: add dialyzer

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Todohai.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
